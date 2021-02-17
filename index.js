9c5497f9610ac3d19084529bb5ff17577e1f1fe8
Yisheng Jiang <yishengjiang@yahoo.com>
2021-02-16 19:11:59 -0800

    fdasfas.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Module from './read.js';
+
 let api;
 let module;
 export async function init() {
@@ -10,40 +11,57 @@ export async function init() {
     };
     api.init();
 }
+
+
 export async function loadSF(presetId, note, velocity, duration) {
     if (!api) {
         await init();
     }
-    console.log("loading " + note);
+    const metaquery =
+        console.log("loading " + note);
     const { _malloc, _free, load_sound, HEAPF32 } = api;
-    const n = duration * 48000;
+    const n = duration * 48000 + 4 + 2;
     const buffer = _malloc(n * Float32Array.BYTES_PER_ELEMENT);
     console.log('ptr' + buffer);
     api.load_sound(buffer, presetId, note, 120, n);
-    const res = new Float32Array(HEAPF32.buffer, buffer, n);
+
+    //    const [pitchRatio, sampleRate, attack, hold, decay, sustain, release, lpf_cf, lpf_q, ...sound] = new Float32Array(HEAPF32.buffer, buffer, n);
+    const r = new Float32Array(HEAPF32.buffer, buffer, n);
+
     _free(buffer);
-    return res; //new Float32Array(Module.HEAPF32.buffer, resPtr, n)
+    return r; //new Float32Array(Module.HEAPF32.buffer, resPtr, n)
 }
 
-const ctx = new AudioContext();
+const ctx = new AudioContext({ sampleRate: 31000 });
 
 for (let i = 0; i < 88; i++) {
     const button = document.createElement("button");
-    button.innerHTML = `${i + 21}`;
+    button.innerHTML = `${i}`;
     let t0;
 
     button.onmousedown = () => {
-        let vel = !t0 ? 50 : (performance.now() - t0) / 100;
-        t0 = performance.now();
-        loadSF(0, i + 21, vel, 0.25).then(buffer => {
-            const ab = new AudioBuffer({ numberOfChannels: 1, length: 25 * 48000, sampleRate: 31000 });
-            ab.copyToChannel(buffer, 0);
 
-            const abs = new AudioBufferSourceNode(ctx, { buffer: ab })
-            abs.connect(ctx.destination);
-            abs.start();
-
-        });
     };
     document.body.append(button);
+}
+
+function play(preset, midi) {
+    let vel = !t0 ? 50 : (performance.now() - t0) / 100;
+    t0 = performance.now();
+
+
+    loadSF(0, i, 55, 1).then(sound => {
+        const ab = new AudioBuffer({ numberOfChannels: 1, length: sound.length, sampleRate: 31000 });
+        ab.copyToChannel(sound, 0);
+        //  console.log(Math.sqrt(sound.reduce((sum, v) => sum += v * v, 0) / 31000));
+        const abs = new AudioBufferSourceNode(ctx, { buffer: ab })
+            //  const g = new GainNode(ctx);
+            // g.gain.exponentialRampToValueAtTime(sustain / 2, Math.pow(2, decay / 12000));
+        abs.connect(ctx.destination);
+
+
+
+        abs.start();
+
+    });
 }
